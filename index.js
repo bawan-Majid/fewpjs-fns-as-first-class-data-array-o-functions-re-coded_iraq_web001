0bbc3e44932f495660e1501c88d16b7169c1d95c
Bawan Majid <bawanmajid23@gmail.com>
2020-05-16 07:04:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function wakeDog(dogName ,dogBreed){
 
 function leashDog(dogName ,dogBreed){
   console.log(`Leash ${dogName} the ${dogBreed}`);
-  let msg=`Leash  ${dogName} the ${dogBreed}`;
+  let msg=`Leash ${dogName} the ${dogBreed}`;
   return msg;
 }
 
